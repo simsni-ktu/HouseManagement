64897ab2aaa30a0a4540b3d645c987b21b37c190
simsni-ktu <simonas.snioka@ktu.edu>
2023-11-13 20:22:00 +0200

    remove guest role

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->enum('role', ['guest', 'user', 'admin']);
+            $table->enum('role', ['user', 'admin'])->default('user');
             $table->timestamps();
         });
     }
